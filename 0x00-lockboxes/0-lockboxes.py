9fa104569fb619c7c725249b4adc6604ee744ba3
AgustinGallotti <97979018+AgustinGallotti@users.noreply.github.com>
2022-12-08 22:03:18 -0300

    unlocked box

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+""" lockboxes - unlock the correct box, Return: True or false """
+
+def canUnlockAll(boxes):
+    """ n number """
+    key = [0]
+
+    for n in key:
+        for correct in boxes[n]:
+            if correct not in key and correct < len(boxes):
+                key.append(correct)
+    if len(key) == len(boxes):
+        return True
+    return False
